53072812e129f71fae43e9086eb9967939257f44
nairod010 <dorianbl@DESKTOP-TCIB0EC>
2024-12-26 23:24:21 +0200

    working on board

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import "./styles.css";
+import { Gameboard } from "./classes/gameboard";
 
-console.log("Webpack template is working!");
+const container = document.querySelector(".main-container");
+const grid = document.querySelector(".grid");
 
+const gameboard = new Gameboard
 
+gameboard.buildBoard()
+
+for (let i = 0; i < gameboard.grid.length; i++) {
+  const cell = document.createElement("div")
+  cell.className = `cell spot-${gameboard.grid[i].index}`
+  cell.innerText = `${gameboard.grid[i].index}`
+  grid.appendChild(cell)
+}
--- webpack.common.js
+++ webpack.common.js
@@ -11,11 +11,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        exclude: ["node_modules"],
-        use: ["babel-loader"],
-      },
-      {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
